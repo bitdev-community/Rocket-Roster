989eafa5ebb6c5560d8b91321fc4c60cd6d7e137
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-07-04 13:31:41 +0200

    Update install command

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
+echo "Installing BVM..."
 npm i -g @teambit/bvm
+echo "Setting BVM to nightly..."
 bvm config set RELEASE_TYPE nightly
+echo "Installing Bit..."
 bvm install
+export PATH=$HOME/bin:$PATH
+echo "Installed Bit version:"
+echo $(bit -v)
 bit install
