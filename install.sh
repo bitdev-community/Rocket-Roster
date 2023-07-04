295f4481378f38e39082fccd72d74b1d2d394906
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-07-04 13:35:25 +0200

    Don't install nightly Bit version

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 echo "Installing BVM..."
 npm i -g @teambit/bvm
-echo "Setting BVM to nightly..."
-bvm config set RELEASE_TYPE nightly
 echo "Installing Bit..."
 bvm install
 export PATH=$HOME/bin:$PATH
