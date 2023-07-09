1757e087dc074de45e7d67726b7cf08909a57c2b
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-07-09 17:39:32 +0200

    Change install.sh commands

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-echo "Installing BVM..."
-npm i -g @teambit/bvm
-echo "Installing Bit..."
-bvm install
-export PATH=$HOME/bin:$PATH
-echo "Installed Bit version:"
-echo $(bit -v)
-bit install
+echo "Checking if Bit is already installed..."
+if ! command -v bit &> /dev/null
+then
+    echo "Bit is not installed, installing BVM and Bit..."
+    npm i -g @teambit/bvm
+    bvm install
+    export PATH=$HOME/bin:$PATH
+else
+    echo "Bit is already installed!"
+fi
+
+echo "Bit version:"
+bit -v
